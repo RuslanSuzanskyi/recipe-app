9b5588d2f424d9da1a2ff326862dcfa40e31ee4e
Ruslan <foyyui@gmail.com>
2025-03-13 17:22:00 +0200

    header input placeholder change

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -44,7 +44,7 @@ export default function Header() {
                 type="text"
                 value={query}
                 onChange={setQuery}
-                placeholder="Search recipes..."
+                placeholder="Search"
                 className="block w-full p-4 ps-6 sm:ps-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
               />
               <button
